0188ff0c15dc8a759162f40f6cc0a1069e636fff
byrainzhi <4571766+ByrainZhi@users.noreply.github.com>
2018-02-07 00:57:30 +0800

    .

--- Classification and Regression Trees/tree.py
+++ Classification and Regression Trees/tree.py
@@ -1,3 +1,4 @@
+
 def gini_index(groups, class_values):
     
     gini = 0.0
@@ -17,7 +18,7 @@ def gini_index(groups, class_values):
 print(gini_index([[[1, 1], [1, 0]], [[1, 1], [1, 0]]], [0, 1]))
 print(gini_index([[[1, 0], [1, 0]], [[1, 1], [1, 1]]], [0, 1]))
 
-#first the score for the worst case at 1.0 followed by the score for the best case at 0.0.
+# first the score for the worst case at 1.0 followed by the score for the best case at 0.0.
 
 # find the best split points
 # A split is comprised of an attribute in the dataset and a value. We can summarize this as the index of an attribute to split and the value by which to split rows on that attribute. 
@@ -42,11 +43,11 @@ def get_split(dataset):
     for index in range(len(dataset[0]) - 1):
         for row in dataset:
             groups = test_split(index, row[index], dataset)
-            print(groups)
-            print('-'*10)
+            #print(groups)
+            #print('-'*10)
             gini = gini_index(groups, class_values)
-            print(gini)
-            print('**'*10)
+            #print(gini)
+            #print('**'*10)
             if gini < b_score:
                 b_index, b_value, b_score, b_groups = index, row[index], gini, groups
     return { 'index':b_index, 'value':b_value, 'groups':b_groups }
@@ -65,3 +66,53 @@ dataset = [[2.771244718,1.784783929,0],
 split = get_split(dataset)
 print(split)
 print('Split: [X%d < %.3f]' % ((split['index']+1), split['value']))
+
+# Build a Tree
+def to_terminal(group):
+    outcomes = [row[-1] for row in group]
+    return max(set(outcomes), key=outcomes.count)
+
+def split(node, max_depth, min_size, depth):
+    left, right = node['groups']
+    del(node['groups'])
+    print(left)
+    print("----")
+    print(right)
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+        return
+    
+    if depth >= max_depth:
+        node['left'], node['right'] = to_terminal(left), to_terminal(right)
+        return
+
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+
+    else:
+        node['left'] = get_split(left)
+        split(node['left'], max_depth, min_size, depth + 1)
+    
+    if len(right) <= min_size:
+        node['right'] = to_terminal(right)
+
+    else:
+        node['right'] = get_split(right)
+        split(node['right'], max_depth, min_size, depth + 1)
+
+def build_tree(train, max_depth, min_size):
+    root = get_split(train)
+    split(root, max_depth, min_size, 1)
+    return root
+
+def print_tree(node, depth = 0):
+    if isinstance(node, dict):
+        print('%s[X%d < %.3f]' % ((depth*'  ', (node['index'] + 1), node['value'])))
+        print_tree(node['left'], depth + 1)
+        print_tree(node['right'], depth + 1)
+    else:
+        print('%s[%s]' % ((depth*'  ', node)))
+
+tree = build_tree(dataset, 3, 1)
+print(tree)
+print_tree(tree)

dc29d27dd8bd886c93b8388c9b12e4fc475c8b65
byrainzhi <4571766+ByrainZhi@users.noreply.github.com>
2018-06-03 21:22:11 +0800

    .

--- Support_Vector_Machine/example.py
+++ Support_Vector_Machine/example.py
@@ -1,18 +1,24 @@
-# coding:UTF-8
-
-import numpy as np
-import algorithm as svm
-
 import os
 
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+
+import algorithm as svm
 from algorithm import *
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+# example 1
+train = [[3, 3], [4, 3], [1, 1]]
+label = [1, 1, -1]
+b, alphas = simpleSMO(train, label, 0.6, 0.001, 1)
+
+print(b)
+print(alphas[alphas > 0])
+
+# example 2
 f = '../Data/svm.csv'
 d = pd.read_csv(f, header=None)
 
@@ -20,7 +26,7 @@ train = d[d.columns[:-1]]
 
 label = d[d.columns[-1]]
 
-b, alphas = smoSimple(train, label, 0.6, 0.001, 40)
+b, alphas = simpleSMO(train, label, 0.6, 0.001, 1)
 
 print(b)
 print(alphas[alphas > 0])

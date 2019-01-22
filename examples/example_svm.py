999445c8f5efd30cf409ab679707e65f50bfba16
byrainzhi <4571766+ByrainZhi@users.noreply.github.com>
2019-01-22 22:52:28 +0800

    .

--- examples/example_svm.py
+++ examples/example_svm.py
@@ -13,15 +13,21 @@ from ml.math_tools import mt
 
 def main():
 
-    # example 1
-    train = [[3, 3], [4, 3], [1, 1]]
-    label = [1, 1, -1]
+    data = pd.read_csv('data/svm.csv', header=None)
+
+    data = np.array(data)
+    X, y = data[:, 0:-1], data[:, -1].astype(int)
 
     svm = support_vector_machine.SVM()
-    b, alphas = svm.simpleSMO(train, label, 1, 0.001, 10000)
 
-    print(b)
-    print(alphas)
+    support_vectors, iterations = svm.fit(X, y)
+
+    sv_count = support_vectors.shape[0]
+    print(sv_count)
+
+    y_hat = svm.predict(X)
+
+    print(svm.calc_acc(y, y_hat))
 
 
 if __name__ == "__main__":
--- ml/support_vector_machine/support_vector_machine.py
+++ ml/support_vector_machine/support_vector_machine.py
@@ -11,111 +11,112 @@ from ml.math_tools import mt
 
 
 class SVM():
-    def selectJrand(self, i, m):
-        j = i  # we want to select any J not equal to i
-        while (j == i):
-            j = int(random.uniform(0, m))
-        return j
 
-    def clipAlpha(self, aj, H, L):
-        if aj > H:
-            aj = H
-        if L > aj:
-            aj = L
-        return aj
+    def __init__(self, max_iter=10000, kernel_type='linear', C=1.0, epsilon=0.001):
+        self.kernels = {
+            'linear': self.kernel_linear,
+            'quadratic': self.kernel_quadratic
+        }
+        self.max_iter = max_iter
+        self.kernel_type = kernel_type
+        self.C = C
+        self.epsilon = epsilon
 
-    def simpleSMO(self, dataMatIn, classLabels, C, toler, maxIter):
-        dataMatrix = np.mat(dataMatIn)
-        # print(dataMatIn)
-        labelMat = np.mat(classLabels).transpose()
-        # print(labelMat)
-        b = 0
-        m, n = np.shape(dataMatrix)  # 100 2
-        alphas = np.mat(np.zeros((m, 1)))  # [100 rows x 1 column's 0]
-        iter = 0
+    def fit(self, X, y):
+        n, d = X.shape[0], X.shape[1]
+        alpha = np.zeros((n))
+        kernel = self.kernels[self.kernel_type]
+        count = 0
+        while True:
+            count += 1
+            alpha_prev = np.copy(alpha)
+            for j in range(0, n):
+                i = self.get_rnd_int(0, n - 1, j)  # Get random int i~=j
+                x_i, x_j, y_i, y_j = X[i, :], X[j, :], y[i], y[j]
+                k_ij = kernel(x_i, x_i) + kernel(x_j, x_j) - \
+                    2 * kernel(x_i, x_j)
+                if k_ij == 0:
+                    continue
+                alpha_prime_j, alpha_prime_i = alpha[j], alpha[i]
+                (L, H) = self.compute_L_H(
+                    self.C, alpha_prime_j, alpha_prime_i, y_j, y_i)
 
-        while (iter < maxIter):
-            alphaPairsChanged = 0
-            for i in range(m):
-                fXi = float(np.multiply(alphas, labelMat).T *
-                            (dataMatrix * dataMatrix[i, :].T)) + b
-                '''
-                print('dataMatrix:{0}'.format(dataMatrix))
-                print('dataMatrix[i, :]:{0}'.format(dataMatrix[i, :]))
-                print('dataMatrix * dataMatrix[i, :].T:{0}'.format(dataMatrix * dataMatrix[i, :].T))
-                '''
+                # Compute model parameters
+                self.w = self.calc_w(alpha, y, X)
+                self.b = self.calc_b(X, y, self.w)
 
-                # if checks if an example violates KKT conditions
-                Ei = fXi - float(labelMat[i])  # 7.105
-                if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
+                # Compute E_i, E_j
+                E_i = self.E(x_i, y_i, self.w, self.b)
+                E_j = self.E(x_j, y_j, self.w, self.b)
 
-                    # random select the second alphas[j]
-                    j = self.selectJrand(i, m)
-                    fXj = float(np.multiply(alphas, labelMat).T *
-                                (dataMatrix * dataMatrix[j, :].T)) + b
-                    Ej = fXj - float(labelMat[j])  # 7.104
-                    alphaIold = alphas[i].copy()
-                    alphaJold = alphas[j].copy()
+                # Set new alpha values
+                alpha[j] = alpha_prime_j + float(y_j * (E_i - E_j)) / k_ij
+                alpha[j] = max(alpha[j], L)
+                alpha[j] = min(alpha[j], H)
 
-                    # make sure alpha is between 0 and C page 126
-                    if (labelMat[i] != labelMat[j]):
-                        L = max(0, alphas[j] - alphas[i])
-                        H = min(C, C + alphas[j] - alphas[i])
-                    else:
-                        L = max(0, alphas[j] + alphas[i] - C)
-                        H = min(C, alphas[j] + alphas[i])
+                alpha[i] = alpha_prime_i + y_i * \
+                    y_j * (alpha_prime_j - alpha[j])
 
-                    if L == H:
-                        print("L==H")
-                        continue
+            # Check convergence
+            diff = np.linalg.norm(alpha - alpha_prev)
+            if diff < self.epsilon:
+                break
 
-                    eta = dataMatrix[i, :] * dataMatrix[i, :].T + dataMatrix[j, :] * \
-                        dataMatrix[j, :].T - 2.0 * dataMatrix[i, :] * \
-                        dataMatrix[j, :].T  # 7.106
+            if count >= self.max_iter:
+                print("Iteration number exceeded the max of %d iterations" %
+                      (self.max_iter))
+                return
+        # Compute final model parameters
+        self.b = self.calc_b(X, y, self.w)
+        if self.kernel_type == 'linear':
+            self.w = self.calc_w(alpha, y, X)
+        # Get support vectors
+        alpha_idx = np.where(alpha > 0)[0]
+        support_vectors = X[alpha_idx, :]
+        return support_vectors, count
 
-                    print('eta:{0}'.format(eta))
+    def predict(self, X):
+        return self.h(X, self.w, self.b)
 
-                    # if eta >= 0, continue
-                    if eta < 0:
-                        print("eta>=0")
-                        continue
+    def calc_b(self, X, y, w):
+        b_tmp = y - np.dot(w.T, X.T)
+        return np.mean(b_tmp)
 
-                    # update alhpas[j]
-                    alphas[j] += labelMat[j] * (Ei - Ej) / eta  # 7.106
-                    alphas[j] = self.clipAlpha(alphas[j], H, L)
+    def calc_w(self, alpha, y, X):
+        return np.dot(X.T, np.multiply(alpha, y))
+    # Prediction
 
-                    if (abs(alphas[j] - alphaJold) < 0.00001):
-                        print("j not moving enough")
-                        continue
+    def h(self, X, w, b):
+        return np.sign(np.dot(w.T, X.T) + b).astype(int)
+    # Prediction error
 
-                    # update i by the same amount as j
-                    alphas[i] += labelMat[j] * labelMat[i] * \
-                        (alphaJold - alphas[j])  # 7.109
+    def E(self, x_k, y_k, w, b):
+        return self.h(x_k, w, b) - y_k
 
-                    # the update is in the oppostie direction
-                    b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[i, :].T \
-                        - labelMat[j] * (alphas[j] - alphaJold) * \
-                        dataMatrix[i, :] * dataMatrix[j, :].T  # 7.115
-                    b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[j, :].T \
-                        - labelMat[j] * (alphas[j] - alphaJold) * \
-                        dataMatrix[j, :] * dataMatrix[j, :].T  # 7.116
+    def compute_L_H(self, C, alpha_prime_j, alpha_prime_i, y_j, y_i):
+        if(y_i != y_j):
+            return (max(0, alpha_prime_j - alpha_prime_i), min(C, C - alpha_prime_i + alpha_prime_j))
+        else:
+            return (max(0, alpha_prime_i + alpha_prime_j - C), min(C, alpha_prime_i + alpha_prime_j))
 
-                    if (0 < alphas[i]) and (C > alphas[i]):
-                        b = b1
-                    elif (0 < alphas[j]) and (C > alphas[j]):
-                        b = b2
-                    else:
-                        b = (b1 + b2) / 2.0
+    def get_rnd_int(self, a, b, z):
+        i = z
+        cnt = 0
+        while i == z and cnt < 1000:
+            i = random.randint(a, b)
+            cnt = cnt + 1
+        return i
+    # Define kernels
 
-                    alphaPairsChanged += 1
-                    print("iter: %d i:%d, pairs changed %d" %
-                          (iter, i, alphaPairsChanged))
+    def kernel_linear(self, x1, x2):
+        return np.dot(x1, x2.T)
 
-            if (alphaPairsChanged == 0):
-                iter += 1
-            else:
-                iter = 0
+    def kernel_quadratic(self, x1, x2):
+        return (np.dot(x1, x2.T) ** 2)
 
-            #print("iteration number: %d" % iter)
-
-        return b, alphas[alphas > 0]
+    def calc_acc(self, y, y_hat):
+        idx = np.where(y_hat == 1)
+        TP = np.sum(y_hat[idx] == y[idx])
+        idx = np.where(y_hat == -1)
+        TN = np.sum(y_hat[idx] == y[idx])
+        return float(TP + TN) / len(y)

336b33934007ab0582d357e1e040aed318c006c9
byrainzhi <4571766+ByrainZhi@users.noreply.github.com>
2019-02-04 19:55:18 +0800

    .

--- examples/example_dt.py
+++ examples/example_dt.py
@@ -4,7 +4,6 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -16,9 +15,7 @@ from ml.math_tools import mt
 def main():
 
     df = pd.read_csv("data/banknote.csv", header=None)
-    train, test = train_test_split(df, test_size=0.4)
-    total = test.shape[0]
-    train, test = train.values.tolist(), test.values.tolist()
+    train, test, test_total = mt.data_train_test(df)
 
     dt = decision_tree.DT()
     tree = dt.build_tree(train, 5, 1)
@@ -30,7 +27,7 @@ def main():
             correct_count += 1
         print('Actual=%d, Predict=%d' % (row[-1], prediction))
 
-    print('{:.2%}'.format(float(correct_count / total)))
+    print('{:.2%}'.format(float(correct_count / test_total)))
 
 
 if __name__ == "__main__":
--- ml/math_tools/mt.py
+++ ml/math_tools/mt.py
@@ -2,6 +2,17 @@ import numpy as np
 import pandas as pd
 import math
 from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+
+
+def data_train_test(df, test_size=0.4):
+    """
+    split data into train and test
+    """
+    train, test = train_test_split(df, test_size=test_size)
+    test_total = test.shape[0]
+    train, test = train.values.tolist(), test.values.tolist()
+    return train, test, test_total
 
 
 def covariance_matrix(X, Y=None):
@@ -17,30 +28,3 @@ def normalize(X, axis=-1, order=2):
     l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
     l2[l2 == 0] = 1
     return X / np.expand_dims(l2, axis)
-
-
-def calculate_entropy(y):
-    """ Calculate the entropy of label array y """
-    def log2(x): return math.log(x) / math.log(2)
-    unique_labels = np.unique(y)
-    entropy = 0
-    for label in unique_labels:
-        count = len(y[y == label])
-        p = count / len(y)
-        entropy += -p * log2(p)
-    return entropy
-
-
-def divide_on_feature(X, feature_i, threshold):
-    """ Divide dataset based on if sample value on feature index is larger than
-        the given threshold """
-    split_func = None
-    if isinstance(threshold, int) or isinstance(threshold, float):
-        def split_func(sample): return sample[feature_i] >= threshold
-    else:
-        def split_func(sample): return sample[feature_i] == threshold
-
-    X_1 = np.array([sample for sample in X if split_func(sample)])
-    X_2 = np.array([sample for sample in X if not split_func(sample)])
-
-    return np.array([X_1, X_2])

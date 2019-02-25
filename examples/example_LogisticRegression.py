cdc78ccc7b51d12b31b6b22c997d8b37b31d502e
byrainzhi <4571766+ByrainZhi@users.noreply.github.com>
2019-02-25 23:57:40 +0800

    .

--- examples/example_LogisticRegression.py
+++ examples/example_LogisticRegression.py
@@ -13,21 +13,23 @@ def main():
     X = iris["data"][:, 3:]  # petal width
     y = (iris["target"] == 2).astype(np.int)  # 1 if Iris-Virginica, else 0
 
-    model = LogisticRegression()
+    model = LogisticRegression(
+        max_iter=5000, learning_rate=0.001, fit_intercept=True)
     model.fit(X, y)
     y_pred = model.predict(X)
 
-    print(y_pred, y)
     accuracy = calculate_accuracy_score(y, y_pred)
     print("Accuracy Score: {:.2%}".format(accuracy))
 
     X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
     y_proba = model.predict_proba(X_new)
-    plt.plot(X_new, y_proba[:, 1], "g-", linewidth=2, label="Iris-Virginica")
-    plt.plot(X_new, y_proba[:, 0], "b--",
+
+    plt.plot(X_new, y_proba[:, 0], "r--",
              linewidth=2, label="Not Iris-Virginica")
-    plt.plot(X[y == 0], y[y == 0], "bs")
-    plt.plot(X[y == 1], y[y == 1], "g^")
+    plt.plot(X_new, y_proba[:, 1], "g-", linewidth=2, label="Iris-Virginica")
+
+    plt.plot(X[y == 0], y[y == 0], "rs")
+    plt.plot(X[y == 1], y[y == 1], "gs")
     plt.legend()
     # plt.show()
     plt.savefig("./examples/example_LogisticRegression.png")
